8ed060d7ae0b64ea43e50bda60dc393ce00e50c1
Conor O'Malley <conor4arms@gmail.com>
2024-07-09 03:52:29 +0000

    k

--- gerber/src/lib.rs
+++ gerber/src/lib.rs
@@ -1,27 +1,59 @@
+//#![feature(trait_alias)]
+
 use std::{collections::HashMap, ptr::NonNull};
+use std::cell::Cell;
 
 use corrida::arena::*;
 
-struct Vertex<Σ> {
+struct Vertex<Σ: Eq + core::hash::Hash + Copy> {
     transitions: HashMap<Σ, NonNull<Vertex<Σ>>>,
     accept: bool
 }
 
-pub struct DFA<'a, Σ> {
+pub struct DFA<'a, Σ: Eq + core::hash::Hash + Copy> {
     arena: Arena<Vertex<Σ>>,
-    start_node: Option<&'a Vertex<Σ>>
+    start_node: Cell<Option<&'a Vertex<Σ>>>
 }
 
-impl<'a, Σ> DFA<'a, Σ> {
-    fn new() -> Self {
+impl<Σ:Eq + core::hash::Hash + Copy> Vertex<Σ> {
+    fn new(accept: bool) -> Self {
         Self {
-            arena: Arena::new(),
-            start_node: None
+            transitions: HashMap::new(),
+            accept
         }
     }
 
-    fn insert_vertex(&self, transitions: &[(Σ, &mut Vertex<Σ> )]) {
-        self.arena.alloc_core()
+    fn set_accept(&mut self, accept: bool) {
+        self.accept = accept;
+    }
+
+    fn append_transitions(&mut self, transitions: &[(Σ, &mut Vertex<Σ> )]){        
+        for (symbol, target_vert) in transitions.iter() {
+            let target_ptr = unsafe {
+                NonNull::new(*target_vert as *const Vertex<Σ> as *mut Vertex<Σ>).unwrap()
+            };
+            self.transitions.insert(*symbol, target_ptr);
+        }
+    }
+}
+
+impl<'a, Σ:Eq + core::hash::Hash + Copy> DFA<'a, Σ> {
+    fn new() -> Self {
+        Self {
+            arena: Arena::new(),
+            start_node: Cell::new(None)
+        }
+    }
+
+    fn insert_vertex(&self, is_accept: bool, transitions: &[(Σ, &mut Vertex<Σ> )]) -> &mut Vertex<Σ> {
+        let mut new_vertex = Vertex::<Σ>::new(is_accept);
+        new_vertex.append_transitions(transitions);
+
+        self.arena.alloc(new_vertex)
+    }
+
+    fn set_start_node(&self) {
+        
     }
 }
 
@@ -31,8 +63,13 @@ mod test{
     use super::DFA;
 
     pub fn test_mixed_symbols() {
-        let dfa = DFA::new();
+        let dfa = DFA::<char>::new();
 
-        let a = );
+        let s_0 = dfa.insert_vertex(true, &[]);
+        dfa.start_node = 
+
+
+
+        //let a = );
     }
 }
